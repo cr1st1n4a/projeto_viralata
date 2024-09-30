1c9be127c50fd82fbda9fc466d18f82492140583
cr1st1n4a <cristina24gs@gmail.com>
2024-09-30 15:09:00 -0400

    att

--- html/arquivos.php
+++ html/arquivos.php
@@ -1,19 +1,18 @@
 /meu_projeto/
   /app/
-    /controllers_models/
+    /controllers/
       AnimalController.php         // Controlador para manipulação de dados de animais (CRUD)
       UsuarioController.php         // Controlador para gerenciamento de usuários e autenticação
       PedidoAdocaoController.php    // Controlador para gerenciar solicitações de adoção
       MedicamentoController.php      // Controlador para manipulação de dados de medicamentos (CRUD)
     /views/
-      index.php                    // Página inicial que exibe a lista de animais
-      criar.php                    // Formulário para cadastrar um novo animal
-      adotar.php                    // Formulário para solicitar a adoção de um animal
-      listar_remedios.php           // Página para exibir a lista de medicamentos disponíveis
-      criar_remedio.php             // Formulário para adicionar um novo medicamento
-      cabecalho.php                 // Arquivo comum para o cabeçalho de todas as páginas
-      rodape.php                    // Arquivo comum para o rodapé de todas as páginas
+      index.html                    // Página inicial que exibe a lista de animais
+      criar.html                    // Formulário para cadastrar um novo animal
+      adotar.html                    // Formulário para solicitar a adoção de um animal
+      listar_remedios.html           // Página para exibir a lista de medicamentos disponíveis
+      criar_remedio.html             // Formulário para adicionar um novo medicamento
+      cabecalho.html                 // Arquivo comum para o cabeçalho de todas as páginas
+      rodape.html                    // Arquivo comum para o rodapé de todas as páginas
   /html/
     index.php                      // Arquivo principal que gerencia o roteamento e inclui as views
-    estilo.css                     // Arquivo opcional para estilos (CSS)
     script.js                      // Arquivo opcional para scripts JavaScript
