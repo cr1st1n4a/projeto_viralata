1af3225f6f691c1c27f3e37cb77bc74c1e7d6d8b
cr1st1n4a <cristina24gs@gmail.com>
2024-09-30 14:31:27 -0400

    1

---
+++ html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require __DIR__ . '/../app/helpers/settings.php';
+require __DIR__ . '/../app/routes/routes.php';
+
+
+$app->run();
