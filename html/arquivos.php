8a44eb6cab9fe09cb030c1c0bf048caeb17edd19
cr1st1n4a <cristina24gs@gmail.com>
2024-09-30 15:16:15 -0400

    att

--- html/arquivos.php
+++ html/arquivos.php
@@ -1,14 +1,14 @@
 /meu_projeto/
   /app/
     /controllers/
-      AnimalController.php         // Controlador para manipulação de dados de animais (CRUD)
+      AnimalController.php  Cristina        // Controlador para manipulação de dados de animais (CRUD)
       UsuarioController.php         // Controlador para gerenciamento de usuários e autenticação
       PedidoAdocaoController.php    // Controlador para gerenciar solicitações de adoção
       MedicamentoController.php      // Controlador para manipulação de dados de medicamentos (CRUD)
     /views/
       login/usuario.html   Welysson
-      index.html                    // Página inicial que exibe a lista de animais
-      criar.html                    // Formulário para cadastrar um novo animal
+      index.html  Cristina                  // Página inicial que exibe a lista de animais
+      criar.html  Cristina                    // Formulário para cadastrar um novo animal
       adotar.html                    // Formulário para solicitar a adoção de um animal
       listar_remedios.html           // Página para exibir a lista de medicamentos disponíveis
       criar_remedio.html             // Formulário para adicionar um novo medicamento
